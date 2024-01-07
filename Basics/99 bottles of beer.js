05e89d7d017d2cc9241d444efea50897c9457f9b
sicktooth <sosaronungwe@gmail.com>
2024-01-07 20:53:52 +0100

    completed a challenge

---
+++ Basics/99 bottles of beer.js
@@ -0,0 +1,13 @@
+var i = 99;
+function beer () {
+    while (i >= 0) {
+        if (i === 0) {
+            console.log ("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+        } else if (i === 1){
+            console.log ("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.");
+        } else {
+            console.log (i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down and pass it around, " + i + " bottles of beer on the wall.");
+         }
+        i--;
+    }
+}
